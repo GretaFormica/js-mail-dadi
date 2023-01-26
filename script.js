6c8359c2698d4e3a5ff6a7cf356b17ea71ec30fe
GretaFormica <greta.formica@gmail.com>
2023-01-26 15:40:03 +0100

    script

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+// dichiarazini costanti globali
 const accedi = document.getElementById('btnAccedi');
 const emailPermesse = ["ciao@gmail.com", "mario@gmail.com", "luigi@gmail.com", "mail@gmail.com"];
 
+
+accedi.addEventListener(
+    'click',
+    
+    function() {
+
+        //dichiarazioni costanti
+        const element = document.getElementById('mail').value;
+        console.log(element)
+
+        let accesso = false;
+
+        for (let i = 0; i < emailPermesse.length; i++) {
+
+            if (element == emailPermesse[i]) {
+                accesso = true;
+            }
+
+        }
+
+        if (accesso == true) {
+            alert("accesso consentito");
+        } else {
+            alert("accesso negato");
+        }
+
+    }
+)
