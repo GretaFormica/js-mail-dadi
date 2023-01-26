d15bb0ad9fed6d03d7e827411546a0c363a53b58
GretaFormica <greta.formica@gmail.com>
2023-01-26 16:13:11 +0100

    DADI: script + final commit

--- dadi/script.js
+++ dadi/script.js
@@ -1,2 +1,32 @@
+//dichiarazioni costanti e variabili globali
+const sfida = document.getElementById('btnSfida');
+
+sfida.addEventListener(
+    'click',
+    
+    function() {
+        let pcNum = Math.floor(Math.random() * 6) + 1;
+        const userNum = Math.floor(Math.random() * 6) + 1;
+
+        if(userNum > pcNum) {
+            //output
+            document.getElementById('risultato').innerHTML= `Complimenti! hai tirato: ${userNum} e il pc ha fatto: ${pcNum}
+            <br>
+            Hai vinto!`;
+        } else if (userNum == pcNum){
+            //output
+            document.getElementById('risultato').innerHTML= `Pareggio! entrambi avete fatto ${pcNum}`;
+        } else {
+            //output
+            document.getElementById('risultato').innerHTML= `Mi dispiace! hai tirato: ${userNum} e il pc ha fatto: ${pcNum}
+            <br>
+            Hai perso`;
+        }
+    }
+
+  
+)
+
+
 
 
